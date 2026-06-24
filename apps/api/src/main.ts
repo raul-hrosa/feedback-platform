2dc519a61be83426cb1b7e4a3d3ea60a2536bc33
Raul Rosa <rosa.hraul@gmail.com>
2026-06-24 15:48:34 -0300

    chore(api): install validation deps and enable ValidationPipe

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,4 +1,4 @@
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 
@@ -6,6 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   const port = process.env.PORT || 3003;
   await app.listen(port);
   Logger.log(`Application is running on: http://localhost:${port}/${globalPrefix}`);
